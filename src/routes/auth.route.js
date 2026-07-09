9e350b9a3323381a53f3f3cb01f08987e9702b3a
sibajit1176 <sibajitmaji2002@gmail.com>
2026-07-09 18:23:24 +0530

    update auth

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,15 @@
 const route=require('express').Router()
 const authcontroller=require('../controllers/auth.controller')
+const authMiddleware = require('../middlewares/auth.middleware')
+
 
 route.post('/register',authcontroller.registerController)
 route.post('/login',authcontroller.logincontroller)
 route.post('/refresh-token',authcontroller.refreshTokenController)
+route.post("/logout",authMiddleware,authcontroller.logoutController);
+route.post("/getProfileDetails",authMiddleware,authcontroller.getProfileDetailsController);
+route.post("/editProfileDetails",authMiddleware,authcontroller.editProfileDetailsController);
+
 
 
 module.exports=route
